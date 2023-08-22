c1d39198dc75957fc750ac995fb415fcd138e81a
mattewkl <cikle290@gmail.com>
2023-08-22 20:14:44 +1000

    big commit, mostly cart

--- JS/input.js
+++ JS/input.js
@@ -23,4 +23,189 @@ function toggleBottomMenu() {
     BOTTOM_MENU.classList.toggle(BOTTOM_MENU_OPENED_CLASS);
 }
 BOTTOMNAV_MENU_BUTTON.addEventListener('click', toggleBottomMenu);
-console.log(BOTTOMNAV_MENU_BUTTON);
+var CART_INFO = {
+    isEmpty: true,
+    total: 0,
+    elements: 0
+};
+var door1 = {
+    name: 'Браво-22 Snow Melinga',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 18600,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Межкомнатная дверь Браво-22 Snow Melinga, no magic fog'
+    }
+};
+var door3 = {
+    name: 'Тестовая дверь',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 25000,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Тестовая дверь'
+    }
+};
+var door4 = {
+    name: 'Тестовая дверь 2',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 250000,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Тестовая дверь'
+    }
+};
+var door2 = {
+    name: 'Браво-22 Snow Melinga / Magic Fog',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door2',
+    class: 'header__cart-item_door2',
+    price: 300600,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-2-full.png',
+        path_webp: './includes/images/header/door-2-full.webp',
+        alt: 'Межкомнатная дверь Браво-22 Snow Melinga'
+    }
+};
+var CART_ITEM_TEMPLATE_SELECTOR = '.header__cart-item-template';
+var CART_ITEM_SELECTOR = '.header__cart-item-start-selector';
+var CART_ITEM_START_CLASS = 'header__cart-item-start-selector';
+var CART_ITEM_TEMPLATE = document.querySelector(CART_ITEM_TEMPLATE_SELECTOR);
+var CART_ITEM_NODE = CART_ITEM_TEMPLATE.content.querySelector(CART_ITEM_SELECTOR).cloneNode(true);
+var ADD_DOOR1_BTN_SELECTOR = '.add-door-1';
+var ADD_DOOR2_BTN_SELECTOR = '.add-door-2';
+var ADD_DOOR3_BTN_SELECTOR = '.add-door-3';
+var ADD_DOOR4_BTN_SELECTOR = '.add-door-4';
+var ADD_DOOR1_BTN = document.querySelector(ADD_DOOR1_BTN_SELECTOR);
+var ADD_DOOR2_BTN = document.querySelector(ADD_DOOR2_BTN_SELECTOR);
+var ADD_DOOR3_BTN = document.querySelector(ADD_DOOR3_BTN_SELECTOR);
+var ADD_DOOR4_BTN = document.querySelector(ADD_DOOR4_BTN_SELECTOR);
+var CARTLIST_SELECTOR = '.header__cart-items';
+var CARTLIST = document.querySelector(CARTLIST_SELECTOR);
+var CART_ITEM_AMOUNT_SELECTOR = '.header__cart-item-amount';
+var CART_ITEM_PRICE_SELECTOR = '.header__cart-item-price';
+var NO_ITEMS_SELECTOR = '.header__cart-item_no-items';
+var CART_TOTAL = document.querySelector('.header__class-total');
+var NO_ITEMS_ELEMENT = CARTLIST.querySelector(NO_ITEMS_SELECTOR);
+function renderCartTotal() {
+    CART_TOTAL.textContent = "".concat(CART_INFO.total);
+}
+function renderItemInCart(cartItemObject) {
+    CART_INFO.isEmpty = false;
+    CART_INFO.elements += 1;
+    console.log(!NO_ITEMS_ELEMENT.classList.contains('d-none'));
+    if (!NO_ITEMS_ELEMENT.classList.contains('d-none')) {
+        NO_ITEMS_ELEMENT.classList.add('d-none');
+    }
+    var cartItemTemplateCopy = CART_ITEM_NODE.cloneNode(true);
+    CARTLIST.appendChild(cartItemTemplateCopy);
+    var cartItem = CARTLIST.querySelector(CART_ITEM_SELECTOR);
+    var cartItemDescriptionElement = cartItem.querySelector('.header__cart-item-description');
+    var cartItemNameElement = cartItem.querySelector('.header__cart-item-name');
+    var cartItemPriceElement = cartItem.querySelector(CART_ITEM_PRICE_SELECTOR);
+    var cartItemPathPNGElement = cartItem.querySelector('.path-png');
+    var cartItemPathWEBPElement = cartItem.querySelector('.path-webp');
+    var cartItemImgElement = cartItem.querySelector('.header__cart-item-image');
+    var cartItemAmountElement = cartItem.querySelector(CART_ITEM_AMOUNT_SELECTOR);
+    var ITEM_DECREASE_BTN = cartItem.querySelector('.header__change-items-amount-button_reduce');
+    var ITEM_ADD_BTN = cartItem.querySelector('.header__change-items-amount-button_add');
+    cartItem.classList.remove(CART_ITEM_START_CLASS);
+    cartItem.classList.add(cartItemObject.class);
+    cartItemNameElement.textContent = cartItemObject.name;
+    cartItemDescriptionElement.textContent = cartItemObject.description;
+    cartItemPriceElement.textContent = "".concat(cartItemObject.price, " \u0420");
+    cartItemPathPNGElement.srcset = cartItemObject.image_info.path;
+    cartItemPathWEBPElement.srcset = cartItemObject.image_info.path_webp;
+    cartItemImgElement.alt = cartItemObject.image_info.alt;
+    cartItemAmountElement.textContent = "".concat(cartItemObject.amount);
+    ITEM_DECREASE_BTN.addEventListener('click', function () { decreaseAmount(cartItemObject); });
+    ITEM_ADD_BTN.addEventListener('click', function () { increaseAmount(cartItemObject); });
+}
+function renderNewItemAmount(amountElement, newAmount) {
+    amountElement.textContent = "".concat(newAmount);
+}
+function renderNewTotalOfItem(priceElement, newTotal) {
+    priceElement.textContent = "".concat(newTotal, " \u0420");
+}
+function decreaseAmount(cartItemObject) {
+    var CART_ITEM_ELEMENT = getCartItem(cartItemObject);
+    var AMOUNT_ELEMENT = CART_ITEM_ELEMENT.querySelector(CART_ITEM_AMOUNT_SELECTOR);
+    var PRICE_ELEMENT = CART_ITEM_ELEMENT.querySelector(CART_ITEM_PRICE_SELECTOR);
+    if (cartItemObject.amount > 1) {
+        cartItemObject.amount -= 1;
+        CART_INFO.total -= cartItemObject.price;
+        renderCartTotal();
+        renderNewTotalOfItem(PRICE_ELEMENT, cartItemObject.price * cartItemObject.amount);
+        renderNewItemAmount(AMOUNT_ELEMENT, cartItemObject.amount);
+    }
+    else if (cartItemObject.amount === 1) {
+        cartItemObject.amount -= 1;
+        CART_INFO.total -= cartItemObject.price;
+        CART_INFO.elements -= 1;
+        if (CART_INFO.elements === 0) {
+            CART_INFO.isEmpty = true;
+            NO_ITEMS_ELEMENT.classList.remove('d-none');
+        }
+        renderCartTotal();
+        CARTLIST.removeChild(CART_ITEM_ELEMENT);
+    }
+    else {
+        return;
+    }
+}
+function increaseAmount(cartItemObject) {
+    var CART_ITEM_ELEMENT = getCartItem(cartItemObject);
+    var AMOUNT_ELEMENT = CART_ITEM_ELEMENT.querySelector(CART_ITEM_AMOUNT_SELECTOR);
+    var PRICE_ELEMENT = CART_ITEM_ELEMENT.querySelector(CART_ITEM_PRICE_SELECTOR);
+    cartItemObject.amount += 1;
+    CART_INFO.total += cartItemObject.price;
+    renderCartTotal();
+    renderNewItemAmount(AMOUNT_ELEMENT, cartItemObject.amount);
+    renderNewTotalOfItem(PRICE_ELEMENT, cartItemObject.price * cartItemObject.amount);
+}
+function getCartItem(cartItemObject) {
+    return document.querySelector(cartItemObject.selector);
+}
+ADD_DOOR1_BTN.addEventListener('click', function () {
+    addItemInCart(door1);
+});
+ADD_DOOR2_BTN.addEventListener('click', function () {
+    addItemInCart(door2);
+});
+ADD_DOOR3_BTN.addEventListener('click', function () {
+    addItemInCart(door3);
+});
+ADD_DOOR4_BTN.addEventListener('click', function () {
+    addItemInCart(door4);
+});
+function addItemInCart(cartItem) {
+    if (cartItem.amount === 0) {
+        cartItem.amount += 1;
+        renderItemInCart(cartItem);
+        CART_INFO.total += cartItem.price;
+        renderCartTotal();
+    }
+    else {
+        var cartItemElement = getCartItem(cartItem);
+        cartItem.amount += 1;
+        var cartItemElementAmount = cartItemElement.querySelector(CART_ITEM_AMOUNT_SELECTOR);
+        var PRICE_ELEMENT = cartItemElement.querySelector(CART_ITEM_PRICE_SELECTOR);
+        renderNewItemAmount(cartItemElementAmount, cartItem.amount);
+        renderNewTotalOfItem(PRICE_ELEMENT, cartItem.amount * cartItem.price);
+        CART_INFO.total += cartItem.price;
+        renderCartTotal();
+    }
+}
--- JS/input.ts
+++ JS/input.ts
@@ -32,4 +32,234 @@ function toggleBottomMenu():void {
 
 BOTTOMNAV_MENU_BUTTON.addEventListener('click', toggleBottomMenu)
 
-console.log(BOTTOMNAV_MENU_BUTTON)
+
+type typeCartItem = {
+    name: string
+    description: string
+    selector: string
+    class: string
+    price: number
+    amount: number
+    image_info: cartImageInfo
+}
+
+type typeCartInfo = {
+    isEmpty: boolean
+    total: number
+    elements: number
+}
+
+const CART_INFO:typeCartInfo = {
+    isEmpty: true,
+    total: 0,
+    elements: 0
+}
+
+type cartImageInfo = {
+    path: string
+    path_webp: string
+    alt: string
+}
+
+let door1:typeCartItem = {
+    name: 'Браво-22 Snow Melinga',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 18600,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Межкомнатная дверь Браво-22 Snow Melinga, no magic fog'
+    }
+}
+let door3:typeCartItem = {
+    name: 'Тестовая дверь',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 25000,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Тестовая дверь'
+    }
+}
+let door4:typeCartItem = {
+    name: 'Тестовая дверь 2',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door1',
+    class: 'header__cart-item_door1',
+    price: 250000,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-1-full.png',
+        path_webp: './includes/images/header/door-1-full.webp',
+        alt: 'Тестовая дверь'
+    }
+}
+
+let door2:typeCartItem = {
+    name: 'Браво-22 Snow Melinga / Magic Fog',
+    description: 'Межкомнатная дверь',
+    selector: '.header__cart-item_door2',
+    class: 'header__cart-item_door2',
+    price: 300600,
+    amount: 0,
+    image_info: {
+        path: './includes/images/header/door-2-full.png',
+        path_webp: './includes/images/header/door-2-full.webp',
+        alt: 'Межкомнатная дверь Браво-22 Snow Melinga'
+    }
+}
+const CART_ITEM_TEMPLATE_SELECTOR:string = '.header__cart-item-template'
+const CART_ITEM_SELECTOR:string = '.header__cart-item-start-selector'
+const CART_ITEM_START_CLASS:string = 'header__cart-item-start-selector'
+const CART_ITEM_TEMPLATE:HTMLTemplateElement = document.querySelector(CART_ITEM_TEMPLATE_SELECTOR)
+const CART_ITEM_NODE = CART_ITEM_TEMPLATE.content.querySelector(CART_ITEM_SELECTOR).cloneNode(true)
+const ADD_DOOR1_BTN_SELECTOR:string = '.add-door-1'
+const ADD_DOOR2_BTN_SELECTOR:string = '.add-door-2'
+const ADD_DOOR3_BTN_SELECTOR:string = '.add-door-3'
+const ADD_DOOR4_BTN_SELECTOR:string = '.add-door-4'
+const ADD_DOOR1_BTN:HTMLElement = document.querySelector(ADD_DOOR1_BTN_SELECTOR)
+const ADD_DOOR2_BTN:HTMLElement = document.querySelector(ADD_DOOR2_BTN_SELECTOR)
+const ADD_DOOR3_BTN:HTMLElement = document.querySelector(ADD_DOOR3_BTN_SELECTOR)
+const ADD_DOOR4_BTN:HTMLElement = document.querySelector(ADD_DOOR4_BTN_SELECTOR)
+const CARTLIST_SELECTOR:string = '.header__cart-items'
+const CARTLIST:HTMLTemplateElement = document.querySelector(CARTLIST_SELECTOR)
+const CART_ITEM_AMOUNT_SELECTOR:string = '.header__cart-item-amount'
+const CART_ITEM_PRICE_SELECTOR:string = '.header__cart-item-price'
+const NO_ITEMS_SELECTOR:string = '.header__cart-item_no-items'
+const CART_TOTAL:HTMLSpanElement = document.querySelector('.header__class-total')
+let NO_ITEMS_ELEMENT:HTMLElement = CARTLIST.querySelector(NO_ITEMS_SELECTOR)
+
+
+
+
+function renderCartTotal():void {
+    CART_TOTAL.textContent = `${CART_INFO.total}`
+}
+
+function renderItemInCart(cartItemObject:typeCartItem):void {
+    CART_INFO.isEmpty = false
+    CART_INFO.elements += 1
+    console.log(!NO_ITEMS_ELEMENT.classList.contains('d-none'))
+    if (!NO_ITEMS_ELEMENT.classList.contains('d-none')) {
+        NO_ITEMS_ELEMENT.classList.add('d-none')
+    }
+    let cartItemTemplateCopy:Node = CART_ITEM_NODE.cloneNode(true)
+    CARTLIST.appendChild(cartItemTemplateCopy)
+    let cartItem:HTMLTemplateElement = CARTLIST.querySelector(CART_ITEM_SELECTOR)
+    let cartItemDescriptionElement:HTMLElement = cartItem.querySelector('.header__cart-item-description')
+    let cartItemNameElement:HTMLElement = cartItem.querySelector('.header__cart-item-name')
+    let cartItemPriceElement:HTMLParagraphElement = cartItem.querySelector(CART_ITEM_PRICE_SELECTOR)
+    let cartItemPathPNGElement:HTMLSourceElement = cartItem.querySelector('.path-png')
+    let cartItemPathWEBPElement:HTMLSourceElement = cartItem.querySelector('.path-webp')
+    let cartItemImgElement:HTMLImageElement = cartItem.querySelector('.header__cart-item-image')
+    let cartItemAmountElement:HTMLParagraphElement = cartItem.querySelector(CART_ITEM_AMOUNT_SELECTOR)
+    const ITEM_DECREASE_BTN = cartItem.querySelector('.header__change-items-amount-button_reduce')
+    const ITEM_ADD_BTN = cartItem.querySelector('.header__change-items-amount-button_add')
+    cartItem.classList.remove(CART_ITEM_START_CLASS)
+    cartItem.classList.add(cartItemObject.class)
+    cartItemNameElement.textContent = cartItemObject.name
+    cartItemDescriptionElement.textContent = cartItemObject.description
+    cartItemPriceElement.textContent = `${cartItemObject.price} Р`
+    cartItemPathPNGElement.srcset = cartItemObject.image_info.path
+    cartItemPathWEBPElement.srcset = cartItemObject.image_info.path_webp
+    cartItemImgElement.alt = cartItemObject.image_info.alt
+    cartItemAmountElement.textContent = `${cartItemObject.amount}`
+    ITEM_DECREASE_BTN.addEventListener('click', () => {decreaseAmount(cartItemObject)})
+    ITEM_ADD_BTN.addEventListener('click', () => {increaseAmount(cartItemObject)})
+}
+
+function renderNewItemAmount(amountElement:HTMLParagraphElement, newAmount:number) {
+    amountElement.textContent = `${newAmount}`
+}
+
+function renderNewTotalOfItem(priceElement: HTMLParagraphElement, newTotal:number) {
+    priceElement.textContent = `${newTotal} Р`
+}
+
+
+
+function decreaseAmount(cartItemObject:typeCartItem):void {
+    const CART_ITEM_ELEMENT:HTMLElement = getCartItem(cartItemObject)
+    const AMOUNT_ELEMENT:HTMLParagraphElement = CART_ITEM_ELEMENT.querySelector(CART_ITEM_AMOUNT_SELECTOR)
+    const PRICE_ELEMENT:HTMLParagraphElement = CART_ITEM_ELEMENT.querySelector(CART_ITEM_PRICE_SELECTOR)
+    if (cartItemObject.amount > 1) {
+        cartItemObject.amount -= 1
+        CART_INFO.total -= cartItemObject.price
+        renderCartTotal()
+        renderNewTotalOfItem(PRICE_ELEMENT, cartItemObject.price * cartItemObject.amount)
+        renderNewItemAmount(AMOUNT_ELEMENT, cartItemObject.amount)
+    }
+    else if (cartItemObject.amount === 1) {
+        cartItemObject.amount -= 1
+        CART_INFO.total -= cartItemObject.price
+        CART_INFO.elements -= 1
+        if (CART_INFO.elements === 0) {
+            CART_INFO.isEmpty = true
+            NO_ITEMS_ELEMENT.classList.remove('d-none')
+        }
+        renderCartTotal()
+        CARTLIST.removeChild(CART_ITEM_ELEMENT)
+    }
+    else {
+        return
+    }
+}
+
+function increaseAmount(cartItemObject:typeCartItem):void {
+    const CART_ITEM_ELEMENT:HTMLElement = getCartItem(cartItemObject)
+    const AMOUNT_ELEMENT:HTMLParagraphElement = CART_ITEM_ELEMENT.querySelector(CART_ITEM_AMOUNT_SELECTOR)
+    const PRICE_ELEMENT:HTMLParagraphElement = CART_ITEM_ELEMENT.querySelector(CART_ITEM_PRICE_SELECTOR)
+    cartItemObject.amount +=1
+    CART_INFO.total += cartItemObject.price
+    renderCartTotal()
+    renderNewItemAmount(AMOUNT_ELEMENT, cartItemObject.amount)
+    renderNewTotalOfItem(PRICE_ELEMENT, cartItemObject.price * cartItemObject.amount)
+}
+function getCartItem(cartItemObject:typeCartItem):HTMLElement {
+    return document.querySelector(cartItemObject.selector)
+}
+
+
+ADD_DOOR1_BTN.addEventListener('click', () => {
+    addItemInCart(door1)
+})
+
+ADD_DOOR2_BTN.addEventListener('click', () => {
+    addItemInCart(door2)
+})
+ADD_DOOR3_BTN.addEventListener('click', () => {
+    addItemInCart(door3)
+})
+ADD_DOOR4_BTN.addEventListener('click', () => {
+    addItemInCart(door4)
+})
+
+
+
+
+function addItemInCart(cartItem:typeCartItem):void {
+    if (cartItem.amount === 0)  {
+        cartItem.amount += 1
+        renderItemInCart(cartItem)
+        CART_INFO.total += cartItem.price
+        renderCartTotal()
+    }
+    else {
+        const cartItemElement:HTMLElement = getCartItem(cartItem)
+        cartItem.amount += 1
+        const cartItemElementAmount:HTMLParagraphElement = cartItemElement.querySelector(CART_ITEM_AMOUNT_SELECTOR)
+        const PRICE_ELEMENT:HTMLParagraphElement = cartItemElement.querySelector(CART_ITEM_PRICE_SELECTOR)
+        renderNewItemAmount(cartItemElementAmount, cartItem.amount)
+        renderNewTotalOfItem(PRICE_ELEMENT, cartItem.amount  * cartItem.price)
+        CART_INFO.total += cartItem.price
+        renderCartTotal()
+    }
+
+
+}
